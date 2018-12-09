bacc240ec97c8c1f417bdab8318abf8c1637525d
Sam (Personal) <samuel.r.marshall@gmail.com>
2018-12-09 11:56:50 -0500

    renamed card path created table component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Table from './Table.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Blackjack React</h1>
+        <Table />
       </div>
     );
   }
---
+++ src/Table.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import './index.css'
+
+class Table extends Component {
+
+  render() {
+    return (<div class="table">table</div>)
+  }
+
+}
+export default Table

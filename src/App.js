e77e531900ffde576681e6342637a7501154497a
Sam (Personal) <samuel.r.marshall@gmail.com>
2018-12-09 20:42:50 -0500

    started controls (no function)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Table from './Table.js'
+import Controls from './Controls.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Blackjack React</h1>
+        <Controls />
         <Table />
       </div>
     );
---
+++ src/Controls.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Controls extends Component {
+
+  render() {
+    return (
+    <div>
+      <button>Deal</button>
+      <button>Hit</button>
+      <button>Stand</button>
+    </div>)
+  }
+
+}
